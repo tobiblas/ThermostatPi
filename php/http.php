b47a645f9e50ff37e90e80462f8570e3b06adfcc
tobiblas <tobias2.jonsson@gmail.com>
2019-03-03 23:37:05 +0100

    http.php again.

--- php/http.php
+++ php/http.php
@@ -1,11 +1,5 @@
 <?php
-
     $request_url = 'http://' . $_GET['url'];
     $contents = file_get_contents($url);
-    if($contents !== false){
-      //Print out the contents.
-      echo $contents;
-    } else {
-      die('Could not fetch ' + $contents);
-    }
+    echo $contents;
 ?>
